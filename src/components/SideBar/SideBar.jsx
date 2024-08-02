2247ae43c7e35a30121785f66b0581bff80caafc
Sonal Sai K S <sonalsai777@gmail.com>
2024-08-01 21:20:41 -0700

    sidebar updated

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,29 +1,28 @@
 import Logo from "../../../src/assets/images/Logo.svg";
-import HomeIcon from '../../../src/assets/images/HomeIcon.svg';
-import InfoIcon from '../../../src/assets/images/InfoIcon.svg';
-import MailIcon from '../../../src/assets/images/MailIcon.svg';
-import ProjectsIcon from '../../../src/assets/images/ProjectsIcon.svg';
-import SkillsIcon from '../../../src/assets/images/SkillsIcon.svg';
+import HomeIcon from "../../../src/assets/images/HomeIcon.svg";
+import InfoIcon from "../../../src/assets/images/InfoIcon.svg";
+import MailIcon from "../../../src/assets/images/MailIcon.svg";
+import ProjectsIcon from "../../../src/assets/images/ProjectsIcon.svg";
+import SkillsIcon from "../../../src/assets/images/SkillsIcon.svg";
 import "./sidebar.scss";
-import ProfileImage from "../../assets/images/Profile.png"
-
+import ProfileImage from "../../assets/images/Profile.png";
 
 function SideBar() {
-    return (
-        <div className="sidebar">
-            <img src={Logo} alt="codeLogo" className="logo" />
-            <div className="options">
-                <img src={HomeIcon} alt="" className="icons" />
-                <img src={InfoIcon} alt="" className="icons" />
-                <img src={MailIcon} alt="" className="icons" />
-                <img src={ProjectsIcon} alt="" className="icons" />
-                <img src={SkillsIcon} alt="" className="icons" />
-            </div>
-            <div className="profile">
-                <img src={ProfileImage} alt="" className="profileImage" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <img src={Logo} alt="codeLogo" className="logo" />
+      <div className="options">
+        <img src={HomeIcon} alt="HomeIcon" className="icons" />
+        <img src={InfoIcon} alt="InfoIcon" className="icons" />
+        <img src={SkillsIcon} alt="SkillsIcon" className="icons" />
+        <img src={ProjectsIcon} alt="ProjectsIcon" className="icons" />
+        <img src={MailIcon} alt="MailIcon" className="icons" />
+      </div>
+      <div className="profile">
+        <img src={ProfileImage} alt="ProfileImage" className="profileImage" />
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
