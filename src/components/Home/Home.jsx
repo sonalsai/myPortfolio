dbb2242c65a10bbb8e8b0729d2947e2d4c20e06c
Sonal Sai K S <sonalsai777@gmail.com>
2024-08-01 22:47:37 -0700

    Home page updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import "./home.scss"
 
 function Home() {
+
+  const hi = `console.log("Hi...");`
+  const name = `() => <name>SONAL SAI</name>`;
+  const description = `Error:404 {meassage:"I'm a Frontend Developer"}`
   return (
     <div className="homeContainer">
-      <h1>Hello</h1>
+      <h1 className="head1">{hi}</h1>
+      <h1 className="head2">{name}</h1>
+      <h3 className="head3">{description}</h3>
     </div>)
 }
 
